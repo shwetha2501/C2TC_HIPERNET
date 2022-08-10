77b3da736f003eb741de5bf5c39fb6bfcad3df10
swetha <shwethabs25@gmail.com>
2022-08-10 20:52:09 +0530

    first commit

---
+++ src/com/cg/service/StudentService.java
@@ -0,0 +1,13 @@
+package com.cg.service;
+
+import com.cg.entities.Student;
+
+public interface StudentService {
+	public abstract void addStudent(Student student);
+	public abstract void updateStudent(Student student);
+	public abstract void removeStudent(Student student);
+
+	public abstract Student findStudentById(int id);
+
+
+}
---
+++ src/com/cg/service/StudentServiceImpl.java
@@ -0,0 +1,43 @@
+package com.cg.service;
+
+import com.cg.dao.StudentDao;
+import com.cg.dao.StudentDaoImpl;
+import com.cg.entities.Student;
+
+public class StudentServiceImpl implements StudentService {
+	
+	private StudentDao dao;
+	
+	public StudentServiceImpl()
+	{
+
+	dao = new StudentDaoImpl();
+	}
+	@Override
+	public void addStudent(Student student) {
+	dao.beginTransaction();
+	dao.addStudent(student);
+	dao.commitTransaction();
+	}
+	@Override
+	public void updateStudent(Student student) {
+	dao.beginTransaction();
+	dao.updateStudent(student);
+	dao.commitTransaction();
+	}
+	@Override
+	public void removeStudent(Student student) {
+	dao.beginTransaction();
+	dao.removeStudent(student);
+	dao.commitTransaction();
+	}
+	@Override
+	public Student findStudentById(int id) {
+	//no need of transaction, as it's an read operation
+	Student student = dao.getStudentById(id);
+	return student;
+	}
+	
+	
+	
+}
